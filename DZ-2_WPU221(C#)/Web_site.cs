ca73ed0ed1d03da76d619bbb1501bcb2dd81fe7d
Хрусталёв Александр <Tikkirey90_90@mail.ru>
2023-02-28 00:11:32 +0300

    Добавьте файлы проекта.

---
+++ DZ-2_WPU221(C#)/Web_site.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Console;
+
+namespace DZ_2_WPU221_C__
+{
+    class Website   //тело класса
+    {
+        public string Name { get; set; }         //Наименование сайта
+        public string Url { get; set; }         //Адрес сайта
+        public string Description{ get; set; }  //Описание сайта
+        public string IP { get; set; }          //IP-адрес сайта
+
+        public Website() //конструктор по умолчанию
+        {
+            Name = "Blank Page";
+            Url = "www.BlankPage.ua";
+            Description = "Этот сайт ещё не наполнен контентом";
+            IP = "127.0.0.1";   
+        }
+
+        public Website (string name, string url, string description, string iP) //перегрузка конструктора
+        {
+            Name = name;
+            Url = url;
+            Description = description;
+            IP = iP;
+        }   
+
+        public void PrintWebSite() //вывод инфы про сайт
+        {
+            WriteLine($"Название сайта: {Name}\nПуть к сайту: {Url}\nОписание сайта: {Description}\nIP-адрес сайта: {IP}\n");
+        }
+
+        public void DataEntry () //попытка сделать ручное заполнение инфы по сайту
+        {
+            //WriteLine("Введите наименование сайта: ");
+            //Name = ReadLine();
+            //WriteLine("Введите адрес сайта: ");
+            //Url = ReadLine();
+            //WriteLine("Введите описание сайта: ");
+            //Description = ReadLine();
+            //WriteLine("Введите IP-адрес сайта: ");
+            //IP = ReadLine();
+        }
+
+
+    }
+    internal class Website_1
+    {
+        static void Main(string[] args)
+        {
+            Website emptyness = new Website();
+            Website weapon = new Website("weapon", "www.weapon.ua", "Сайт по продаже оружия", "127.0.0.12");
+            
+            emptyness.PrintWebSite();
+            weapon.PrintWebSite();
+    
+        }
+    }
+}
